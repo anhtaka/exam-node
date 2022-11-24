7dae2e637c2c301b2aa3b0453cb2f191e1c4f01d
tytaka <tytakayanagi@nissho-ele.co.jp>
2022-11-24 17:36:29 +0700

    add

---
+++ random-cat/src/pages/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const result = (await res.json()) as SearchCatImageResponse;
+    return result[0];
+};
+
+fetchCatImage().then((image) => {
+  console.log(`猫の画像: ${image.url}`);
+});
+
+///
+const IndexPage = () => {
+    const [catImageUrl, setCatImageUrl] = useState(
+        "https://cdn2.thecatapi.com/images/bpc.jpg"
+      );
+     
+      const handleClick = async () => {
+        const image = await fetchCatImage();
+        setCatImageUrl(image.url);
+      };
+
+  return (
+    <div>
+      <button onClick={handleClick}>きょうのにゃんこ🐱</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl}  width={500} height="auto" />
+      </div>
+    </div>
+  );
+};
+ 
+export default IndexPage;
+
+
+interface CatCategory {
+    id: number;
+    name: string;
+}
+interface SearchCatImage {
+    breeds: string[];
+    categories: CatCategory[];
+    id: string;
+    url: string;
+    width: number;
+    height: number;
+}
+   
+  type SearchCatImageResponse = SearchCatImage[];
+
+
+
